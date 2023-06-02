120cf0290e1bee0362088f0f0890f83ea419947a
andriy <evicandrij@gmail.com>
2023-06-02 15:53:10 +0300

    +comit

---
+++ js/01-gallery.js
@@ -0,0 +1,41 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+
+console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+console.log(galleryEl);
+const markapGalery = galleryItems
+  .map(
+    ({ description, original, preview }) =>
+      `<li class="gallery__item">
+  <a class="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`
+  )
+  .join("");
+galleryEl.insertAdjacentHTML("beforeend", markapGalery);
+galleryEl.addEventListener("click", onClickGalery);
+function onClickGalery(e) {
+  e.preventDefault();
+  if (e.target.nodeName !== "IMG") {
+    return;
+  }
+  const getgetAttribut = e.target.getAttribute("data-source");
+  const instance = basicLightbox.create(`
+    <img src="${getgetAttribut}" width="800" height="600">
+`);
+
+  instance.show();
+  window.addEventListener("keydown", (e) => {
+    if (e.key === "Escape") {
+      instance.close();
+      console.log("helow vasa");
+    }
+  });
+}
---
+++ js/02-lightbox.js
@@ -0,0 +1,57 @@
+// import { galleryItems } from './gallery-items.js';
+// // Change code below this line
+
+
+// const galleryEl = document.querySelector(".gallery");
+// const markapGalery = createGallery(galleryItems);
+//     function createGallery(galleryItems) {
+//         return galleryItems.map(
+//             ({ description, original, preview }) =>
+//                 `<li class="gallery__item">
+//   <a class="${original}">
+//     <img
+//       class="gallery__image"
+//       src="${preview}"
+//       data-source="${original}"
+//       alt="${description}"
+//     />
+//   </a>
+// </li>`).join("");
+//     };
+// console.log(createGallery(galleryItems));
+// galleryEl.insertAdjacentHTML("beforeend", markapGalery);
+
+
+// new SimpleLightbox(".gallery a", {
+//   captionsData: "alt",
+//   captionDelay: 250,
+// });
+
+
+
+
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+
+const gallery = document.querySelector(".gallery");
+const items = [];
+
+galleryItems.map((e) => {
+  const galleryLink = document.createElement("a");
+  galleryLink.className = "gallery__link";
+  galleryLink.href = e.original;
+  const galleryImage = document.createElement("img");
+  galleryImage.className = "gallery__image";
+  galleryImage.src = e.preview;
+  galleryImage.setAttribute("title", e.description);
+  galleryImage.alt = e.description;
+
+  galleryLink.append(galleryImage);
+  items.push(galleryLink);
+});
+gallery.append(...items);
+
+new SimpleLightbox(".gallery a", {
+  captionDelay: 250,
+  captionsData: "alt",
+});
